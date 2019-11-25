553bdc7ad84bc7011acaf303a923c828b4c7de72
Joseph <osekuemen@gmail.com>
2019-11-25 10:38:53 +0100

    Preferences setup with functionality

--- app/src/main/java/com/example/earthquake/EarthquakeListFragment.java
+++ app/src/main/java/com/example/earthquake/EarthquakeListFragment.java
@@ -1,7 +1,9 @@
 package com.example.earthquake;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,24 +22,28 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class EarthquakeListFragment extends Fragment {
 
+    protected EarthquakeViewModel earthquakeViewModel;
     private SwipeRefreshLayout mSwipeToRefreshView;
-    protected  EarthquakeViewModel earthquakeViewModel;
     private RecyclerView mRecyclerView;
     private ArrayList<Earthquake> mEarthquakes = new ArrayList<Earthquake>();
     private EarthquakeRecyclerViewAdapter mEarthquakeAdapter = new EarthquakeRecyclerViewAdapter(mEarthquakes);
+    private OnListFragmentInteractionListener mListener;
+    // This method reads the Shared Preference minimum magnitude value
+    private int mMinMagnitude = 0;
 
-    public EarthquakeListFragment(){}
+    public EarthquakeListFragment() {
+    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_earthquake_list, container, false);
 
-        mRecyclerView = (RecyclerView)view.findViewById(R.id.list);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.list);
         mSwipeToRefreshView = view.findViewById(R.id.swiperefresh);
         return view;
     }
@@ -54,7 +60,7 @@ public class EarthquakeListFragment extends Fragment {
             @Override
             public void onChanged(List<Earthquake> earthquakes) {
                 // When the View Model changes update the list
-                if (earthquakes != null){
+                if (earthquakes != null) {
                     setEarthquakes(earthquakes);
                 }
             }
@@ -75,28 +81,40 @@ public class EarthquakeListFragment extends Fragment {
         });
     }
 
-
     // An earthquake method that takes a list of Earthquakes, checks for duplicates, and then adds
     // each new Earthquake to the Array list. It should also notify the Recycler View Adapter that
     // a new item has been inserted
-    public void setEarthquakes(List<Earthquake>earthquakes){
-        for(Earthquake earthquake: earthquakes){
-            if(!mEarthquakes.contains(earthquake)){
-                mEarthquakes.add(earthquake);
-                mEarthquakeAdapter.notifyItemInserted(mEarthquakes.indexOf(earthquake));
+    public void setEarthquakes(List<Earthquake> earthquakes) {
+        updateFromPreferences();
+
+        for (Earthquake earthquake : earthquakes) {
+            if (earthquake.getMagnitude() >= mMinMagnitude) {
+                if (!mEarthquakes.contains(earthquake)) {
+                    mEarthquakes.add(earthquake);
+                    mEarthquakeAdapter.notifyItemInserted(mEarthquakes.indexOf(earthquake));
+                }
             }
         }
+
+        // if the earthquake list derived has a value that is greater than what the user has keyed in in the preferences,
+        // remove it.
+        if (mEarthquakes != null && mEarthquakes.size() > 0)
+            for (int i = mEarthquakes.size() - 1; i >= 0; i--) {
+                if (mEarthquakes.get(i).getMagnitude() < mMinMagnitude) {
+                    mEarthquakes.remove(i);
+                    mEarthquakeAdapter.notifyItemRemoved(i);
+                }
+            }
         // This method disables the "refreshing" visual indicator when an update has been received.
         // This is because the update itself will be performed by the Earthquake View Model, which
         // will communicate through the parent Activity.
         mSwipeToRefreshView.setRefreshing(false);
-    }
 
-    public interface OnListFragmentInteractionListener{
-        void onListFragmentRefreshRequested();
-    }
+        // Apply the magnitude filter by updating the setEarthquakes method from the EarthquakeListFragment
+        // to update the minimum magnitude preference, and check each earthquake's magnitude before
+        // adding it to the list:
 
-    private OnListFragmentInteractionListener mListener;
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -110,11 +128,45 @@ public class EarthquakeListFragment extends Fragment {
         mListener = null;
     }
 
-    protected void updateEarthquakes(){
-        if(mListener !=null){
+    protected void updateEarthquakes() {
+        if (mListener != null) {
             mListener.onListFragmentRefreshRequested();
         }
+    }
 
+    private void updateFromPreferences() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mMinMagnitude = Integer.parseInt( // CONVERT THE VALUE GOTTEN FROM THE USER CLICK TO AN INTEGER
+                prefs.getString(PreferencesActivity.PREF_MIN_MAG, "3"));
+    }
+
+    // This step is to create a new OnSharedPreferenceChangeListener within the EarthquakeListFragment
+    // that will repopulate the Earthquake list, applying the magnitude filter based on the new
+    // setting
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Register an OnSharedPreferenceChangeListener
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        prefs.registerOnSharedPreferenceChangeListener(mPrefListener);
+    }
+
+    
+    private SharedPreferences.OnSharedPreferenceChangeListener mPrefListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if(PreferencesActivity.PREF_MIN_MAG.equals(key)){
+                List<Earthquake>earthquakes = earthquakeViewModel.getEarthquakes().getValue();
+                if(earthquakes !=null){
+                    setEarthquakes(earthquakes);
+                }
+            }
+        }
+    };
+
+    public interface OnListFragmentInteractionListener {
+        void onListFragmentRefreshRequested();
     }
 
 
